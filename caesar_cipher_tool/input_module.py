65939efd13bbb03fc0ad1b83403e20b7cacde656
SURESH KUMAR <45168608+Jsuresh47@users.noreply.github.com>
2025-07-10 06:09:07 +0530

    Update input_module.py

--- caesar_cipher_tool/input_module.py
+++ caesar_cipher_tool/input_module.py
@@ -1,4 +1,6 @@
 def get_input():
+    mode = input("Choose mode (E = Encrypt, D = Decrypt): ").strip().upper()
     text = input("Enter the text: ")
     key = int(input("Enter the shift key: "))
-    return text, key
+    return mode, text, key
+
